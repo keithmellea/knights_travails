ad9f9de9e2344c3830502c2d8f262a52ce3919e4
Caro <55109108+CaroMen@users.noreply.github.com>
2021-05-06 13:15:47 -0500

    work before lunch break

--- tree.py
+++ tree.py
@@ -1,9 +1,8 @@
 class Node:
-    def __init__(self, value): 
+    def __init__(self, value):
         self._value = value
         self._parent = None
         self._children = []
-    
 
     @property
     def value(self):
@@ -13,6 +12,7 @@ class Node:
     def children(self):
         return self._children
 
-    def add_child(self):
-        self._children.append(self)
-        if self not in self._children
+    def add_child(self, node):
+        self._children.append(node)
+        if node not in self._children:
+            self._parent = node
