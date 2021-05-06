00fa441d418e86791b0bd9327d758cf0a3b36064
Keith Mellea <keithmellea@gmail.com>
2021-05-06 15:58:20 -0400

    still on phase 1

--- tree.py
+++ tree.py
@@ -12,10 +12,18 @@ class Node:
     def children(self):
         return self._children
 
+    @property
+    def parent(self):
+        return self._parent
+
     def add_child(self, node):
         self._children.append(node)
         if node not in self._children:
             self._parent = node
-
+    
     def add_parents(self):
-        return self._parent._value
+        return self._parent
+
+    @parent.setter
+    def parent(self, node):
+       return self._parent.add_child(self)
