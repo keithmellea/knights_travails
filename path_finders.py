9eb78681e6d4ac6ed1ed857f9b0060998aad1171
Keith Mellea <keithmellea@gmail.com>
2021-05-07 12:28:16 -0400

    working on phase 2

--- path_finders.py
+++ path_finders.py
@@ -3,5 +3,23 @@ import Node
 
 class KnightPathFinder:
     def __init__(self, (0, 0)):
-        self.root = (0, 0)
-        self.considered_positions = set(self.root)
+        self._root = (0, 0)
+        self._considered_positions = set(self.root)
+
+    def get_valid_moves(self, pos): 
+        possible_moves = [
+        (-2, -1),
+        (-2, 1),
+        (-1, -2),
+        (-1, 2),
+        (1, -2),
+        (1, 2),
+        (2, -1),
+        (2, 1),
+    ]
+        valid_moves = []
+            for move in possible_moves:
+                new_position = tuple(lambda i, j: i + j, pos, move)
+                if new_position[0] in range(0, 7) and if new_position[1] in range(0, 7): 
+                    valid_moves.append(new_position)
+        return valid_moves
